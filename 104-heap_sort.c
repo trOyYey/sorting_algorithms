4d815a019fe73cf296081863681376e3a710a371
trOyYeyY <troyzuwara@gmail.com>
2024-01-23 23:58:50 +0000

    adding swap function for checker

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,6 +1,21 @@
 #include "sort.h"
 
 /**
+ * swap - swap between two variables
+ * @var1: the first variable
+ * @var2: the second variable
+ */
+
+void swap(int *var1, int *var2)
+{
+        int temp;
+
+        temp = *var1;
+        *var1 = *var2;
+        *var2 = temp;
+}
+
+/**
  * heap_sort - fxn sorts an arr using the sift-down algorithm heap sort
  * @array: the given array
  * @size: the sizemofmthe array
